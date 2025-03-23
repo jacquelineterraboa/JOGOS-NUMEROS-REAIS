3a55fa9707333435cf89060806813916a7f4c3e0
JACQUELINE GENOVEZ <167133184+jacquelineterraboa@users.noreply.github.com>
2025-03-23 14:15:50 +0000

    finalização calculos

--- perguntas.js
+++ perguntas.js
@@ -1,47 +1,119 @@
 criaCartao(
-    'Química', 
-    'O que é um átomo?', 
-    'Um átomo é a menor unidade de um elemento químico'
+    'PERGUNTA 01', 
+    'CALCULE',
+    'para x = 3 <br>3x + 5x − 2 + 7'
 )
 
 criaCartao(
-    'Biologia', 
-    'Qual é a função do coração?', 
-    'O coração bombeia sangue para todo o corpo'
+    'PERGUNTA 02', 
+    'CALCULE',
+    'para x = 2 <br>4x + 3x − 3 + 8'
 )
 
 criaCartao(
-    'Geometria', 
-    'O que é um triângulo?', 
-    'Um triângulo é um polígono com três lados'
+    'PERGUNTA 03', 
+    'CALCULE',
+    'para x = 4 <br>2x + 3x − 4 + 9'
 )
 
 criaCartao(
-    'História', 
-    'Quem foi Cleópatra?', 
-    'Cleópatra foi a última faraó do Egito Antigo'
+    'PERGUNTA 04', 
+    'CALCULE',
+    'para x = 2 <br>4x + 6x − 5 + 12'
 )
 
 criaCartao(
-    'Astronomia', 
-    'O que é o sistema solar?', 
-    'O sistema solar é composto pelo Sol e todos os corpos que orbitam ao seu redor'
+    'PERGUNTA 05', 
+    'CALCULE',
+    '3² + 3³ ='
 )
 
 criaCartao(
-    'Português', 
-    'O que é um verbo?', 
-    'Verbo é uma palavra que indica ação, estado ou fenômeno'
+    'PERGUNTA 06', 
+    'CALCULE',
+    '3² + 2³ - 2² ='
 )
 
 criaCartao(
-    'Programação', 
-    'O que é uma variável?', 
-    'Uma variável é um espaço na memória que armazena valores que podem mudar'
+    'PERGUNTA 07', 
+    'CALCULE',
+    '5² + 3³ + 4² ='
 )
 
 criaCartao(
-    'Educação Física', 
-    'Qual é o benefício do alongamento?', 
-    'O alongamento ajuda a melhorar a flexibilidade e prevenir lesões'
+    'PERGUNTA 08', 
+    'CALCULE',
+    '6² + 4² - 2³ ='
+)
+
+criaCartao(
+    'PERGUNTA 09', 
+    'CALCULE',
+    '5³ - 6² ='
+)
+
+criaCartao(
+    'PERGUNTA 10', 
+    'CALCULE',
+    '4³ - 3³ ='
+)
+
+criaCartao(
+    'PERGUNTA 11', 
+    'CALCULE',
+    '7² + 4² ='
+)
+
+criaCartao(
+    'PERGUNTA 12', 
+    'CALCULE',
+    'para x = 3 <br> 2x + 3x + 2² + 3²'
+)
+
+criaCartao(
+    'PERGUNTA 13', 
+    'CALCULE',
+    'para x = 4 <br> 4x + 2x + 3² + 2³='
+)
+
+criaCartao(
+    'PERGUNTA 14', 
+    'CALCULE',
+    'para x = 5 <br> 5x + 2x + 3² - 2³ ='
+)
+
+criaCartao(
+    'PERGUNTA 15', 
+    'CALCULE',
+    'para x = 6 <br> 5x + 2x + 3³ - 3 ='
+)
+
+criaCartao(
+    'PERGUNTA 16', 
+    'CALCULE',
+    'para x = 4 <br> 8x + 4x + 8 - 2³='
+)
+
+criaCartao(
+    'PERGUNTA 17', 
+    'CALCULE',
+    'para x = 5 <br> 4x + 10x + 3³ - 3²'
+)
+
+criaCartao(
+    'PERGUNTA 18', 
+    'CALCULE',
+    'para x = 3 <br> 10x + 6x + 5² - 6²'
+)
+
+criaCartao(
+    'PERGUNTA 19', 
+    'CALCULE',
+    '10² - 5³ ='
+)
+
+criaCartao(
+    'PERGUNTA 20', 
+    'CALCULE',
+    '8² + 5³ - 10² - 3³='
 )
